d9b6a17da64c1639ba050be7fe632588b1af90d7
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-21 11:29:44 +0200

    recuperation id

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+const nomUtilisateur  = document.querySelector('#nomUtilisateur');
 const btnLogin        = document.querySelector('#btnLogin');
 const btnDéconnextion = document.querySelector('#btnDéconnextion');
 const contenuWeb      = document.querySelector('#bcontenuWeb');
 const text            = document.querySelector('#text');
+const formulaire      = document.querySelector('#formulaire');
+
 
 
