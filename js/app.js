9970676567af1e83476f4f6000dd508f5d7c66a5
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-21 14:27:57 +0200

    creations de fonctions chat

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const nomUtilisateur  = document.querySelector('#nomUtilisateur');
 const btnLogin        = document.querySelector('#btnLogin');
 const btnDéconnextion = document.querySelector('#btnDéconnextion');
-const contenuWeb      = document.querySelector('#bcontenuWeb');
+const contenuWeb      = document.querySelector('#contenuWeb');
 const text            = document.querySelector('#text');
 const formulaire      = document.querySelector('#formulaire');
 
@@ -9,16 +9,46 @@ const formulaire      = document.querySelector('#formulaire');
 
 firebase.auth().onAuthStateChanged((user) => { //Définir un observateur d'état d'authentification et obtenir les données utilisateur. 
   if (user) {
+    console.log(user)
     actionFermerSession()
+    nomUtilisateur.innerHTML = user.displayName; //proprieté de l'objet 'user'
+    chatContenu(user)
+
   } else {
     actionAcces()
+    nomUtilisateur.innerHTML = 'I-Chat';
   }
 });
 
+const chatContenu = (user)=> {
+  formulaire.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    
+  })
+}
+
+const actionAcces = ()=> {
+  formulaire.classList.add('d-none'); 
+  contenuWeb.innerHTML = `<p class="lead mt-5 text-center">Initialiser session</p>`
+
+  btnLogin.addEventListener('click', async()=>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+    try {
+      await firebase.auth().signInWithPopup(provider)
+    } catch (error) {
+      console.log(error)
+      
+    }
+  })
+}
+
 const actionFermerSession = ()=> {
+  formulaire.classList.remove('d-none'); 
+  btnDéconnextion.addEventListener('click', ()=>{
+    firebase.auth().signOut()
+  })
+
 
   }
 
-const actionAcces = ()=> {
-  
-}
+
