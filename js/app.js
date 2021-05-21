1e75c9a4b208b91137760ef0ad493df0e31e36b8
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-21 11:25:46 +0200

    recuperation id

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log('hello')
+const btnLogin        = document.querySelector('#btnLogin');
+const btnDéconnextion = document.querySelector('#btnDéconnextion');
+const contenuWeb      = document.querySelector('#bcontenuWeb');
+const text            = document.querySelector('#text');
+
+
